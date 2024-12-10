27a02ecd45df673fafce2571bd876f6886daea6d
Jonathan Preetham <125488951+JP-Repository@users.noreply.github.com>
2024-12-10 17:49:57 +0530

    Updated on 10/12/2024

--- User Scripts/Set-ADUsers_BulkAttributeUpdate.ps1
+++ User Scripts/Set-ADUsers_BulkAttributeUpdate.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    Bulk update attributes for AD users.
+    Update any attribute for Active Directory Users in Bulk
 
 .DESCRIPTION
     This script updates specified attributes for multiple Active Directory users in bulk.
@@ -11,7 +11,9 @@
     Author         : [Your Name]
     Approved By    : [Approver's Name]
     Date           : [Date]
-    Purpose        : The purpose of the script is to perform bulk updates of a specific attribute for multiple Active Directory user accounts by reading the attribute values from a CSV file. This automation simplifies and accelerates the process of modifying AD user attributes, especially when managing a large number of users, ensuring accuracy and consistency while reducing manual effort.
+    Purpose        : The purpose of the script is to perform bulk updates of a specific attribute for multiple Active Directory user accounts,
+                     by reading the attribute values from a CSV file. This automation simplifies and accelerates the process of modifying AD user attributes, 
+                     especially when managing a large number of users, ensuring accuracy and consistency while reducing manual effort.
 
 .PREREQUISITES
     - Active Directory PowerShell module installed.
@@ -19,17 +21,20 @@
     - Input file with required details (e.g., CSV format).
 
 .PARAMETERS
-    [Optional] Define any script parameters here if applicable.
+    None
 
 .EXAMPLE
-    Provide an example of how to use the script.
+    - Create a new Excel file and type SamAccountName in Cell A1, Type "AttributeValue" in Cell B1,
+    - Add the list of users to whom the attribute needs to be updated,
+    - Run the Script in Administrator Mode,
+    - It can be any attribute that you want to update, Description, Office, DisplayName or any new custom attribute.
 
 #>
 
 # Start of Script
 
 # Update Attribute Name
-$AttributeName = "pilotUser"
+$AttributeName = "Description"
 
 # Import Active Directory Module
 Import-Module ActiveDirectory -ErrorAction Stop
